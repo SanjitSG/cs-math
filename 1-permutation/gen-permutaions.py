21cd9aa557ff664176fadac781854e55cd6124da
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2026-02-05 11:35:38 +0530

    Implement function to calculate unique permutations and update print statements

--- 1-permutation/gen-permutaions.py
+++ 1-permutation/gen-permutaions.py
@@ -1,6 +1,26 @@
 from itertools import permutations
+from math import factorial
+from collections import defaultdict
 
-#perm = [''.join(x) for x in list(permutations("barbarian"))]
-perm = {''.join(x) for x in list(permutations("barbarian"))} #set {} notation to eliminate the repetition
-print(perm)
-print(len(perm))
+# perm = [''.join(x) for x in list(permutations("barbarian"))]
+#perm = {''.join(x) for x in list(permutations("electrocardiagraphic"))} #set {} notation to eliminate the repetition
+#print(perm)
+#print(len(perm))
+
+
+def how_many_permutations(text: str) -> int:
+  letters = defaultdict(int)
+  for letter in text:
+      letters[letter] += 1
+   
+  numerator = factorial(len(text))
+  factors = [factorial(count) for count in letters.values()]
+  denominator = 1
+  for factor in factors:
+    denominator *= factor
+  return numerator // denominator
+
+print(how_many_permutations("electrocargafiographic"))
+#print(list(map(''.join, permutations("ab"))))
+
+#print(factorial(9))
